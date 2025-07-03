234adf2b11ce88eac0448f19aa74b292d8aefe12
sadiq-sabo-aliyu <sadiqsabo123@gmail.com>
2025-07-03 09:12:25 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function showContact() {
+    const contactDiv = document.getElementById("contact-info");
+    contactDiv.style.display = contactDiv.style.display === "block" ? "none" : "block";
+  }
